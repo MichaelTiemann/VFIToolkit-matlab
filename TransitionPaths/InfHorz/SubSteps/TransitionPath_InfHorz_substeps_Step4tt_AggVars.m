cc98ce9f765bcdf2aef3f773bcb0aebc65453f94
Michael Tiemann <mdtiemann@gmail.com>
2026-04-26 17:11:18 +1200

    Fix missing ';'
    End line with semicolon so AggVars doesn't print every evalution.

--- TransitionPaths/InfHorz/SubSteps/TransitionPath_InfHorz_substeps_Step4tt_AggVars.m
+++ TransitionPaths/InfHorz/SubSteps/TransitionPath_InfHorz_substeps_Step4tt_AggVars.m
@@ -2,7 +2,7 @@ function AggVars=TransitionPath_InfHorz_substeps_Step4tt_AggVars(AgentDist,Polic
 % Maybe should just take PolicyValuesPath_tt as input instead of PolicyValuesPath
 
 % Note we are passing `AggVarNames` into a function expecting `FnsToEvaluateNames`.  The aggvar names are indeed identical to their function's names.
-AggVars=EvalFnOnAgentDist_InfHorz_TPath_SingleStep_AggVars(AgentDist, PolicyValuesPath_tt, FnsToEvaluateCell, Parameters, FnsToEvaluateParamNames, AggVarNames, n_a, n_z, a_gridvals, z_gridvals, 1)
+AggVars=EvalFnOnAgentDist_InfHorz_TPath_SingleStep_AggVars(AgentDist, PolicyValuesPath_tt, FnsToEvaluateCell, Parameters, FnsToEvaluateParamNames, AggVarNames, n_a, n_z, a_gridvals, z_gridvals, 1);
 
 
 
