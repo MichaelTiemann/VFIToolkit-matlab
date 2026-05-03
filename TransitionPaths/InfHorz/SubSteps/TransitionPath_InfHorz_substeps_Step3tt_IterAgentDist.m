f4dba1109a8802082604170fabba3a5956c03abf
Michael Tiemann <mdtiemann@gmail.com>
2026-05-03 17:18:54 +1200

    Fixes and cleanups
    Fix problems with tminus1, exp asset being silently ignored in agg var evaluation, and clean up some typos.

--- TransitionPaths/InfHorz/SubSteps/TransitionPath_InfHorz_substeps_Step3tt_IterAgentDist.m
+++ TransitionPaths/InfHorz/SubSteps/TransitionPath_InfHorz_substeps_Step3tt_IterAgentDist.m
@@ -8,7 +8,7 @@ else
     N_z=1;
 end
 
-if N_probs==0
+if isempty(N_probs) || N_probs==0
     AgentDistnext=AgentDist_InfHorz_TPath_SingleStep(AgentDist,PolicyPath_ForAgentDistIter(:,tt),II1,II2,N_a,N_z,pi_z);
 else
     AgentDistnext=AgentDist_InfHorz_TPath_SingleStep_nProbs(AgentDist,PolicyPath_ForAgentDistIter(:,tt),II2,PolicyProbsPath(:,tt),N_a,N_z,pi_z);
