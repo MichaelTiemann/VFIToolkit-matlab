38585afe7eb68394f188fe0817c0f3500f60dd4e
Michael Tiemann <mdtiemann@gmail.com>
2026-06-16 09:06:41 +1200

    Restore lowmemory=3 in one case

--- ValueFnIter/FHorz/ExperienceAsset/ValueFnIter_FHorz_ExpAsset_e_raw.m
+++ ValueFnIter/FHorz/ExperienceAsset/ValueFnIter_FHorz_ExpAsset_e_raw.m
@@ -23,6 +23,10 @@ if vfoptions.lowmemory==1
 elseif vfoptions.lowmemory==2
     special_n_e=ones(1,length(n_e),vfoptions.precision);
     special_n_z=ones(1,length(n_z),vfoptions.precision);
+elseif vfoptions.lowmemory==3
+    special_n_e=ones(1,length(n_e),vfoptions.precision);
+    special_n_z=ones(1,length(n_z),vfoptions.precision);
+    special_n_ea=ones(1,length(n_a2),vfoptions.precision);
 end
 
 %% j=N_j
@@ -56,6 +60,21 @@ if ~isfield(vfoptions,'V_Jplus1')
                 [Vtemp,maxindex]=max(ReturnMatrix_ze);
                 V(:,z_c,e_c,N_j)=Vtemp;
                 Policy(:,z_c,e_c,N_j)=maxindex;
+            end
+        end
+    elseif vfoptions.lowmemory==3
+        for ea_c=1:N_a2
+            ea_val=a2_gridvals(ea_c);
+            for z_c=1:N_z
+                z_val=z_gridvals_J(z_c,:,N_j);
+                for e_c=1:N_e
+                    e_val=e_gridvals_J(e_c,:,N_j);
+                    ReturnMatrix_ea_ze=CreateReturnFnMatrix_ExpAsset_Disc_e(ReturnFn, n_d1,n_d2,n_a1,n_a1, special_n_ea, special_n_z, special_n_e, d_gridvals, a1_gridvals, a1_gridvals, ea_val, z_val, e_val, ReturnFnParamsVec,0,0); % Level=0, Refine=0
+                    % Calc the max and its index
+                    [Vtemp,maxindex]=max(ReturnMatrix_ea_ze);
+                    V(1+(ea_c-1)*N_a1:ea_c*N_a1,z_c,e_c,N_j)=Vtemp;
+                    Policy(1+(ea_c-1)*N_a1:ea_c*N_a1,z_c,e_c,N_j)=maxindex;
+                end
             end
         end
     end
@@ -132,6 +151,26 @@ else
 
                 V(:,z_c,e_c,N_j)=shiftdim(Vtemp,1);
                 Policy(:,z_c,e_c,N_j)=shiftdim(maxindex,1);
+            end
+        end
+    elseif vfoptions.lowmemory==3
+        for ea_c=1:N_a2
+            ea_val=a2_gridvals(ea_c);
+            for z_c=1:N_z
+                z_val=z_gridvals_J(z_c,:,N_j);
+                DiscountedEV_ea_z=DiscountFactorParamsVec*repelem(EV(:,ea_c,z_c),N_d1,N_a1);
+                for e_c=1:N_e
+                    e_val=e_gridvals_J(e_c,:,N_j);
+                    ReturnMatrix_ea_ze=CreateReturnFnMatrix_ExpAsset_Disc_e(ReturnFn, n_d1,n_d2,n_a1,n_a1,special_n_ea,special_n_z,special_n_e, d_gridvals, a1_gridvals, a1_gridvals, ea_val, z_val, e_val, ReturnFnParamsVec,0,0); % Level=0, Refine=0
+
+                    entireRHS_ea_z=ReturnMatrix_ea_ze+DiscountedEV_ea_z;
+
+                    % Calc the max and its index
+                    [Vtemp,maxindex]=max(entireRHS_ea_z,[],1);
+
+                    V(1+(ea_c-1)*N_a1:ea_c*N_a1,z_c,e_c,N_j)=shiftdim(Vtemp,1);
+                    Policy(1+(ea_c-1)*N_a1:ea_c*N_a1,z_c,e_c,N_j)=shiftdim(maxindex,1);
+                end
             end
         end
     end
@@ -221,6 +260,27 @@ for reverse_j=1:N_j-1
 
                 V(:,z_c,e_c,jj)=shiftdim(Vtemp,1);
                 Policy(:,z_c,e_c,jj)=shiftdim(maxindex,1);
+            end
+        end
+    elseif vfoptions.lowmemory==3
+        for ea_c=1:N_a2
+            ea_val=a2_gridvals(ea_c);
+            for z_c=1:N_z
+                z_val=z_gridvals_J(z_c,:,jj);
+                DiscountedEV_ea_z=DiscountFactorParamsVec*repelem(EV(:,ea_c,z_c),N_d1,N_a1);
+                for e_c=1:N_e
+                    e_val=e_gridvals_J(e_c,:,jj);
+
+                    ReturnMatrix_ea_ze=CreateReturnFnMatrix_ExpAsset_Disc_e(ReturnFn, n_d1,n_d2,n_a1,n_a1,special_n_ea,special_n_z,special_n_e, d_gridvals, a1_gridvals, a1_gridvals, ea_val, z_val, e_val, ReturnFnParamsVec,0,0); % Level=0, Refine=0
+
+                    entireRHS_ea_z=ReturnMatrix_ea_ze+DiscountedEV_ea_z;
+
+                    %Calc the max and its index
+                    [Vtemp,maxindex]=max(entireRHS_ea_z,[],1);
+
+                    V(1+(ea_c-1)*N_a1:ea_c*N_a1,z_c,e_c,jj)=shiftdim(Vtemp,1);
+                    Policy(1+(ea_c-1)*N_a1:ea_c*N_a1,z_c,e_c,jj)=shiftdim(maxindex,1);
+                end
             end
         end
     end
