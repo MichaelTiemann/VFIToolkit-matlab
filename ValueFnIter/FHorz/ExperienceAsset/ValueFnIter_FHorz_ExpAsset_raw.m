579bcd077e398beff21268d3d735daf718d9a3ea
Michael Tiemann <mdtiemann@gmail.com>
2026-07-07 05:16:48 +1200

    Manually merge in lowmemory==3 changes

--- ValueFnIter/FHorz/ExperienceAsset/ValueFnIter_FHorz_ExpAsset_raw.m
+++ ValueFnIter/FHorz/ExperienceAsset/ValueFnIter_FHorz_ExpAsset_raw.m
@@ -15,6 +15,8 @@ a2_gridvals=CreateGridvals(n_a2,a2_grid,1);
 
 if vfoptions.lowmemory==1
     special_n_z=ones(1,length(n_z),vfoptions.precision);
+elseif vfoptions.lowmemory==2
+    error("invalid vfoptions.lowmemory without e");
 elseif vfoptions.lowmemory==3
     special_n_z=ones(1,length(n_z),vfoptions.precision);
     special_n_ea=ones(1,length(n_a2),vfoptions.precision);
@@ -75,7 +77,9 @@ else
     aprimeProbs(skipinterp)=0; % effectively skips interpolation
 
     % Switch EV from being in terms of a2prime to being in terms of d2 and a2
-    EV=aprimeProbs.*Vlower+(1-aprimeProbs).*Vupper; % (d2,a1prime,a2,u,zprime)
+    EV_l=aprimeProbs.*Vlower; EV_u=(1-aprimeProbs).*Vupper;
+    EV_l(isnan(EV_l))=0; EV_u(isnan(EV_u))=0;
+    EV=EV_l+EV_u; % (d2*a1prime,a2,zprime)
     % Already applied the probabilities from interpolating onto grid
 
     EV=EV.*shiftdim(pi_z_J(:,:,N_j)',-2);
@@ -149,25 +153,31 @@ for reverse_j=1:N_j-1
     [a2primeIndex,a2primeProbs]=CreateExperienceAssetFnMatrix(aprimeFn, n_d2, n_a2, d2_gridvals, a2_grid, aprimeFnParamsVec,2); % Note, is actually aprime_grid (but a_grid is anyway same for all ages)
     % Note: aprimeIndex is [N_d2,N_a2], whereas aprimeProbs is [N_d2,N_a2]
 
-    aprimeIndex=repelem((1:1:N_a1)',N_d2,N_a2)+N_a1*repmat(a2primeIndex-1,N_a1,1,1); % [N_d2*N_a1,N_a2]
-    aprimeplus1Index=repelem((1:1:N_a1)',N_d2,N_a2)+N_a1*repmat(a2primeIndex,N_a1,1,1); % [N_d2*N_a1,N_a2]
-    aprimeProbs=repmat(a2primeProbs,N_a1,1,N_z); % [N_d2*N_a1,N_a2,N_z]
-
-    Vlower=reshape(V(aprimeIndex(:),:,jj+1),[N_d2*N_a1,N_a2,N_z]);
-    Vupper=reshape(V(aprimeplus1Index(:),:,jj+1),[N_d2*N_a1,N_a2,N_z]);
-    % Skip interpolation when upper and lower are equal (otherwise can cause numerical rounding errors)
-    skipinterp=(Vlower==Vupper);
-    aprimeProbs(skipinterp)=0; % effectively skips interpolation
-
-    % Switch EV from being in terms of a2prime to being in terms of d2 and a2
-    EV=aprimeProbs.*Vlower+(1-aprimeProbs).*Vupper; % (d2,a1prime,a2,zprime)
-    % Already applied the probabilities from interpolating onto grid
-
-    % EV is over (d2,a1prime,a2,zprime)
-    EV=EV.*shiftdim(pi_z_J(:,:,jj)',-2);
-    EV(isnan(EV))=0; % remove nan created where value fn is -Inf but probability is zero
-    EV=squeeze(sum(EV,3));
-    % EV is over (d2,a1prime,a2,z)
+    if vfoptions.lowmemory<3
+        aprimeIndex=repelem((1:1:N_a1)',N_d2,N_a2)+N_a1*repmat(a2primeIndex-1,N_a1,1); % [N_d2*N_a1,N_a2]
+        aprimeplus1Index=repelem((1:1:N_a1)',N_d2,N_a2)+N_a1*repmat(a2primeIndex,N_a1,1); % [N_d2*N_a1,N_a2]
+        aprimeProbs=repmat(a2primeProbs,N_a1,1,N_z); % [N_d2*N_a1,N_a2,N_z]
+    
+        Vlower=reshape(V(aprimeIndex(:),:,jj+1),[N_d2*N_a1,N_a2,N_z]);
+        Vupper=reshape(V(aprimeplus1Index(:),:,jj+1),[N_d2*N_a1,N_a2,N_z]);
+        % Skip interpolation when upper and lower are equal (otherwise can cause numerical rounding errors)
+        skipinterp=(Vlower==Vupper);
+        aprimeProbs(skipinterp)=0; % effectively skips interpolation
+    
+        % Switch EV from being in terms of a2prime to being in terms of d2 and a2
+        EV_l=aprimeProbs.*Vlower; EV_u=(1-aprimeProbs).*Vupper;
+        EV_l(isnan(EV_l))=0; EV_u(isnan(EV_u))=0;
+        EV=EV_l+EV_u; % (d2*a1prime,a2,zprime)
+        % Already applied the probabilities from interpolating onto grid
+    
+        % EV is over (d2,a1prime,a2,zprime)
+        EV=EV.*shiftdim(pi_z_J(:,:,jj)',-2);
+        EV(isnan(EV))=0; % remove nan created where value fn is -Inf but probability is zero
+        EV=squeeze(sum(EV,3));
+        % EV is over (d2,a1prime,a2,z)
+    else
+        % We will compute EV piece by piece
+    end
 
     % DiscountedEV=DiscountFactorParamsVec*repelem(EV,N_d1,N_a1);
 
@@ -199,13 +209,34 @@ for reverse_j=1:N_j-1
 
     elseif vfoptions.lowmemory==3
         for ea_c=1:N_a2
+            aprimeIndex_ea=repelem((1:1:N_a1)',N_d2,1)+N_a1*repmat(squeeze(a2primeIndex(:,ea_c))-1,N_a1,1); % [N_d2*N_a1,1]
+            aprimeplus1Index_ea=repelem((1:1:N_a1)',N_d2)+N_a1*repmat(squeeze(a2primeIndex(:,ea_c)),N_a1,1); % [N_d2*N_a1,1]
+            aprimeProbs_ea=repmat(squeeze(a2primeProbs(:,ea_c)),N_a1,1,N_z); % [N_d2*N_a1,1,N_z]
+
+            Vlower_ea=reshape(V(aprimeIndex_ea(:),:,jj+1),[N_d2*N_a1,1,N_z]); % (d2*a1prime,1,z)
+            Vupper_ea=reshape(V(aprimeplus1Index_ea(:),:,jj+1),[N_d2*N_a1,1,N_z]);
+            % Skip interpolation when upper and lower are equal (otherwise can cause numerical rounding errors)
+            skipinterp_ea=(Vlower_ea==Vupper_ea);
+            aprimeProbs_ea(skipinterp_ea)=0; % effectively skips interpolation
+
+            % Switch EV from being in terms of a2prime to being in terms of d (and the present ea_c, from a2)
+            EV_ea_l=aprimeProbs_ea.*Vlower_ea; EV_ea_u=(1-aprimeProbs_ea).*Vupper_ea;
+            EV_ea_l(isnan(EV_ea_l))=0; EV_ea_u(isnan(EV_ea_u))=0;
+            EV_ea=EV_ea_l+EV_ea_u; % (d2*a1prime,1,z)
+            % Already applied the probabilities from interpolating onto grid
+
+            EV_ea=EV_ea.*shiftdim(pi_z_J(:,:,jj),-1); % shifted pi_z shaped [1,z,zprime] -- no transpose since current z is dim 3
+            EV_ea(isnan(EV_ea))=0; % remove nan created where value fn is -Inf but probability is zero
+            EV_ea=squeeze(sum(EV_ea,3));
+            % EV is over (d2*a1prime,z)
+
             ea_val=a2_gridvals(ea_c);
             for z_c=1:N_z
                 z_val=z_gridvals_J(z_c,:,jj);
 
                 ReturnMatrix_ea_z=CreateReturnFnMatrix_ExpAsset_Disc(ReturnFn, n_d1,n_d2,n_a1,n_a1,special_n_ea,special_n_z, d_gridvals, a1_gridvals, a1_gridvals, ea_val, z_val, ReturnFnParamsVec,0,0); % Level=0, Refine=0
 
-                entireRHS_ea_z=ReturnMatrix_ea_z+DiscountFactorParamsVec*repelem(EV(:,ea_c,z_c),N_d1,N_a1);
+                entireRHS_ea_z=ReturnMatrix_ea_z+DiscountFactorParamsVec*repelem(EV_ea(:,z_c),N_d1,N_a1);
 
                 %Calc the max and its index
                 [Vtemp,maxindex]=max(entireRHS_ea_z,[],1);
