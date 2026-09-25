0d704927e9e785be6fe96b253a10f752b3bbea3a
Michael Tiemann <mdtiemann@gmail.com>
2026-09-26 05:59:26 +1200

    DC2A maths fixed, but speed is broken

--- ValueFnIter/FHorz/DivideConquer/ValueFnIter_DC2A_Slicer.m
+++ ValueFnIter/FHorz/DivideConquer/ValueFnIter_DC2A_Slicer.m
@@ -58,13 +58,14 @@ for ii = 1:(num_anchors - 1)
     seg_state_chunk = seg_state_chunk(:)';
     num_seg = length(segment_a1_states);
 
-    loweredge_a1 = repmat(Pol_a1_anch_for_gap(ii, :, :, :), [num_seg, 1, 1, 1]);
-    loweredge_a1 = permute(loweredge_a1, [2, 1, 3, 4]); % [N_a2_endo, num_seg, N_other_states, N_ze]
-    loweredge_a1 = reshape(loweredge_a1, [N_a2_endo, num_seg * N_other_states, N_ze]);
+    % CRITICAL FIX: Strictly preserve all 5 dimensions during extraction!
+    % Pol_a1_anch_for_gap is [num_anchors, N_choice_a1_dc, N_a2_endo, N_other_states, N_ze]
+    loweredge_a1 = repmat(Pol_a1_anch_for_gap(ii, :, :, :, :), [num_seg, 1, 1, 1, 1]);
+    loweredge_a1 = permute(loweredge_a1, [2, 3, 1, 4, 5]); % [N_choice_a1_dc, N_a2_endo, num_seg, N_other_states, N_ze]
+    loweredge_a1 = reshape(loweredge_a1, [N_choice_a1_dc, N_a2_endo, num_seg * N_other_states, N_ze]);
 
     if maxgap(ii) > 0
-        % CRITICAL FIX: Bound the grid against N_a1_dc (the actual grid length),
-        % NOT N_choice_a1_dc (which we repurposed to carry N_d_safe = 1).
+        % Bound the grid against N_a1_dc (the actual grid length)
         loweredge_a1 = min(loweredge_a1, N_a1_dc);
         upper_bound_req = loweredge_a1 + maxgap(ii);
         mg_eval = max(maxgap(ii), max(upper_bound_req - loweredge_a1, [], 'all'));
